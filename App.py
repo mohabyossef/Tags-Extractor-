67faf1afee7f9999061cb366a1699d33221d3798
mohabyossef <mohabyossef1993@gmail.com>
2026-04-22 08:26:09 +0200

    Update App.py

--- App.py
+++ App.py
@@ -60,7 +60,7 @@ if check_password():
         st.rerun()
 
     # --- MAIN MODULE: MENU TAGGER ---
-    st.title("🏷️ Hobz AI Menu Tagger")
+    st.title("🏷️ Hobz Menu Tagger")
     blacklist, clean_tags = load_tagging_resources()
     
     col1, col2 = st.columns([1, 2])
